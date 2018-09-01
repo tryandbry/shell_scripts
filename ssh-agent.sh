64cb21e888316b29e953e85bbfbe853fa78f8b2d
Bryan <bryan@localhost.localdomain>
2018-08-31 20:38:54 -0500

    initial commit

---
+++ ssh-agent.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SOCKET_FILE=$HOME/.ssh_sock/ssh-agent-socket
+
+function start_ssh_agent {
+  eval `ssh-agent -s -a $SOCKET_FILE`;
+  ssh-add;
+}
+
+function cleanup {
+  if [[ -e $SOCKET_FILE ]]; then
+    rm $SOCKET_FILE;
+  fi
+  AGENT_PID=`ps -ef | awk '/ssh-agent.*ssh-agent-socket$/ {print $2}'`
+  if [[ $AGENT_PID =~ [0-9]+ ]]; then
+    kill -9 `ps -ef | awk '/ssh-agent.*ssh-agent-socket$/ {print $2}'`
+  fi
+}
+
+# find existing PID, if any:
+AGENT_PID=`ps -ef | awk '/ssh-agent.*ssh-agent-socket$/ {print $2}'`
+
+if [[ $AGENT_PID =~ [0-9]+ && -e $SOCKET_FILE ]]; then
+  SSH_AGENT_PID=$AGENT_PID; export SSH_AGENT_PID;
+  SSH_AUTH_SOCK=$SOCKET_FILE; export SSH_AUTH_SOCK;
+  echo "ssh agent PID ${SSH_AGENT_PID} found.  Attaching to existing process"
+else
+  echo 'cleaning up and starting new ssh agent'
+  cleanup
+  start_ssh_agent
+fi
